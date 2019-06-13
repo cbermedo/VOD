9dfcf7b55b52993331b771ad6310d21ac7ffbbea
cristian bermedo <cristianbermedog@gmail.com>
2019-06-13 00:11:43 -0400

    Added initial load
    Added VOD.Common - with the entities
    Added VOD.Database - with the context and migration
    Added VOD.UI - With login and register UI

---
+++ VOD.Database/Contexts/VODContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VOD.Common.Entities;
+
+namespace VOD.Database.Contexts
+{
+    public class VODContext : IdentityDbContext<VODUser>
+    {
+        public VODContext(DbContextOptions<VODContext> options): base (options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ VOD.UI/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+//Agregado para DI (dependency injection)
+using VOD.UI.Models;
+using Microsoft.AspNetCore.Identity;
+using VOD.Common.Entities;
+
+namespace VOD.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //DI Login 
+        private SignInManager<VODUser> _signInManager;
+        public HomeController(SignInManager<VODUser> signInMgr)
+        {
+            _signInManager = signInMgr;
+        }
+
+        public IActionResult Index()
+        {
+            if (!_signInManager.IsSignedIn(User))
+                return RedirectToPage("/Account/Login", new { Area = "Identity" });
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
